bd8a534fa1803a47c2051c401f1136faac9089fe
Rochety <Rochety@DESKTOP-AVMJ5D9>
2020-04-11 12:14:25 +0200

    Agregar archivos de proyecto.

---
+++ ENTITY/LiquidacionSubsidiada.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public class LiquidacionSubsidiada : LiquidacionCuotaModeradora
+    {
+        
+
+        public LiquidacionSubsidiada(int numerodeLiquidacion, string identificacion, decimal valordeServicio) : 
+            base(numerodeLiquidacion, identificacion, "subsidiada", 0, valordeServicio)
+        {
+        }
+       
+        public override void EstablecerTarifa()
+        {
+            Tarifa = (decimal)0.05;
+           
+        }
+
+        public override void EstablecerTopemaximo()
+        {
+            TopeMaximo = 200000;
+
+        }
+    }
+}
+
